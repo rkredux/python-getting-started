f7ee4d833e512d569f834646d5b194bf1ad8dfa3
Rahul <rkredux@gmail.com>
2017-12-17 22:11:06 -0500

    Setting Up Python

---
+++ flow.py
@@ -0,0 +1,6 @@
+x = int(input("enter an integer:  "))
+if x < 0: 
+	x = 0
+	print("Negative number changed to 0")
+
+	
---
+++ input.py
@@ -0,0 +1,2 @@
+name = input("Name: ")
+print("Hey, My name is {0}".format(name))
